f4e5d626145b08b898d45684b0642f294dfca400
Jose Martinez <josealbertomartinez.vi@gmail.com>
2022-10-11 23:57:26 -0500

    adding checks on the table

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
 import { People } from '@/data';
+import { Person } from '@/models';
+import { Checkbox } from '@mui/material';
 
 export interface HomeInterface { }
 
 const Home: React.FC<HomeInterface> = () => {
 
+	const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
 	const pageSize: number = 5;
+
+	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id);
+	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id);
+
+	const handleChange = (person: Person) => {
+		setSelectedPeople(findPerson(person) ? filterPerson(person) : [...selectedPeople, person]);
+	};
+
 	const columns = [
+		{
+			field: 'actions',
+			headerName: '',
+			type: 'actions',
+			sortable: false,
+			width: 50,
+			renderCell: (params: GridRenderCellParams) => <>{
+				<Checkbox 
+					size='small' 
+					checked={findPerson(params.row)}
+					onChange={() => handleChange(params.row)}
+				/>
+			}</>
+		},
 		{
 			field: 'name',
 			headerName: 'Name',
 			flex: 1,
 			minWidth: 150,
-			renderCell: (paramns: GridRenderCellParams) => <>{paramns.value}</>
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
 		},
 		{
 			field: 'category',
 			headerName: 'Category',
 			flex: 1,
 			minWidth: 150,
-			renderCell: (paramns: GridRenderCellParams) => <>{paramns.value}</>
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
 		},
 		{
 			field: 'company',
 			headerName: 'Company',
 			flex: 1,
 			minWidth: 150,
-			renderCell: (paramns: GridRenderCellParams) => <>{paramns.value}</>
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
 		}
 	];
 
@@ -40,7 +65,7 @@ const Home: React.FC<HomeInterface> = () => {
 			pageSize={pageSize}
 			rows={People}
 			rowsPerPageOptions={[pageSize]}
-			getRowId={(row: any) => row.id}
+			getRowId={(row: Person) => row.id}
 		/>
 	);
 };
